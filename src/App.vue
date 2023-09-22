62567cbdf2244ee3ac8bc43a1aa1c4aeead54630
Matteo M <74145047+Matteo0099@users.noreply.github.com>
2023-09-22 22:46:55 +0200

    first commit 😁

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <ul>
+      <li v-for="user in users" :key="user.id"> 
+        {{ user.name }} <!--display results-->
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+  import axios from "axios";
+
+  export default {
+    data() {
+      return { users: [] }  // variables
+    },
+    async mounted() {
+      try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users"); //example api
+        this.users = response.data; //update the users data 
+      } catch (e) { console.log(e) } // error handling
+    }
+  }
+</script>
