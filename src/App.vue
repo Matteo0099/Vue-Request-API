59717ae75c38b9043de23a83c81b5bff8223bdaf
Matteo M <74145047+Matteo0099@users.noreply.github.com>
2023-10-24 16:28:28 +0200

    fix link

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <main>
     <ul>
-      <li v-for="user in users" :key="user.id"> 
-        {{ user.name }} <!--display results-->
-      </li>
+      <li v-for="user in users" :key="user.id"> ID: {{ user.id }} </li>
+      <li v-for="user in users" :key="user.uid"> UID: {{ user.uid }} </li>
+      <li v-for="user in users" :key="user.city"> City: {{ user.city }} </li>
+      <li v-for="user in users" :key="user.street_name"> Street Name: {{ user.street_name }} </li>
+      <li v-for="user in users" :key="user.street_address"> Street Address: {{ user.street_address }} </li>
     </ul>
   </main>
 </template>
 
 <script>
-  import axios from "axios";
-
-  export default {
-    data() {
-      return { users: [] }  // variables
-    },
-    async mounted() {
-      try {
-        const response = await axios.get("https://jsonplaceholder.typicode.com/users"); //example api
-        this.users = response.data; //update the users data 
-      } catch (e) { console.log(e) } // error handling
+import axios from "axios";
+export default {
+  data() {
+    return { users: [] };
+  },
+  async mounted() {
+    try {
+      const response = await axios.get("https://random-data-api.com/api/address/random_address");
+      this.users = [response.data];
+    } catch (e) {
+      console.log(e); //Error handling
     }
   }
+};
 </script>
